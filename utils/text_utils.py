7b4179d2fe2f2dcfdcfdec345877d6d88e8ce076
Geetesh Singh <singh.geetesh1998@gmail.com>
2026-07-07 20:44:26 +0530

    feat: build keyword index using dictionaries

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,19 +1,38 @@
-def clean_query(query: str) -> str:
-   query = query.strip().lower().replace("?", "")
-   words = query.split()
-   stop_words = ["what", "is", "the", "are", "do", "to", "and", "a", "an"]
-   cleansed_words = []
-   for word in words:
-        if word.lower() not in stop_words:
-            if words.index(word) == 0:
-                cleansed_words.append(word.lower())
-            else:
-                cleansed_words.append(word)
-   return " ".join(cleansed_words)
+def clean_query(query: str) -> list[str]:
+   normalized = query.strip().lower().replace("?", "").replace(".", "").replace(",", "")
+   words = normalized.split()
+   stop_words = {"what", "is", "the", "are", "do", "to", "and", "a", "an"}   
+   cleansed_words = [word for word in words if word not in stop_words]
+   return cleansed_words
 
 input_query = "What is Apache Kafka?"
 output = clean_query(input_query)
 print(output)
+
+paragraphs = [
+    "Apache Kafka is an event streaming platform.",
+    "Spark processes big data.",
+    "Apache Kafka integrates with Spark."
+]
+
+index = {}
+for paragraph_index, text in enumerate(paragraphs):
+    
+    cleaned_words = clean_query(text)
+    
+    for word in cleaned_words:
+        
+        if word not in index:
+            index[word] = [paragraph_index]
+        else:
+            if paragraph_index not in index[word]:
+                index[word].append(paragraph_index)
+
+print("--- Dynamic Keyword Index Completed ---")
+print(index)
+# import pprint
+# pprint.pprint(index)
+
 # def clean_query(query: str) -> str:
 #     query = query.strip().lower().replace("?", "")
 #     words = query.split()
